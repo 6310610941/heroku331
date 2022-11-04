13b51c4d536587da937c65797e2aa9dbd8f81221
Onjira Wongpolganan <6310611022@cn103>
2022-11-04 18:45:35 +0700

    add index view main

--- bts_for_fun/views.py
+++ bts_for_fun/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+from .models import Station
 # Create your views here.
 
 def index(request):
-    pass
+    stations = Station.objects.all()
+    return render(request, 'bts_for_fun/index.html',{
+                   'stations' : stations
+        
+    })
+        
+    
