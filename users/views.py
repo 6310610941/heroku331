feaec0072c5206a3af07d93de8aca1940ed16a58
Phattarakorn Kaewchukul <6310611030@cn103>
2022-11-04 23:31:34 +0700

    add login

--- users/views.py
+++ users/views.py
@@ -1,13 +1,35 @@
 
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
 
 import users
 
 # Create your views here.
-
+log_in = 'users/login.html'
 
 def index(request):
-    return render(request, "users/index.html", {
-    })
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('users:login'))
+    return render(request, 'users/index.html')
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('users:index'))
+        else:
+            return render(request, log_in, {
+                'message': 'Invalid credentials.'
+            })
+    return render(request, log_in)
+
+def logout_view(request):
+    logout(request)
+    return render(request, log_in, {
+                'message': 'You are logged out.'
+            })
