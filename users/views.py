2abff2e799647040229d850ac779d99269eac1c8
6310682593 <6310682593@student.tu.ac.th>
2022-11-05 15:28:31 +0700

    update

--- users/views.py
+++ users/views.py
@@ -39,8 +39,10 @@ def signup_view(request):
     if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-        
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
         return render(request, log_in)
     else:
         form = SignUpForm()
