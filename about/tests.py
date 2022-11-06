034c683920faa1abe90d0032ffb26a9a339d9c5a
Chanathinad Phispratuang <6310610941@cn103>
2022-11-06 17:55:19 +0700

    test about us

--- about/tests.py
+++ about/tests.py
@@ -13,3 +13,10 @@ class StationTestCase(TestCase):
         c = Client()
         response = c.get(reverse('about:index'))
         self.assertEqual(response.status_code, 200)
+
+    def test_valid_about_us_page(self):
+        """ valid about us page should return status code 200 """
+
+        c = Client()
+        response = c.get(reverse('about:about_us'))
+        self.assertEqual(response.status_code, 200)
