68749bade098fc81c80c475e32f9366c8c917a05
Phattarakorn Kaewchukul <6310611030@cn103>
2022-11-12 18:23:38 +0700

    add edit profile

--- users/forms.py
+++ users/forms.py
@@ -11,3 +11,12 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'name', 'surname', 'email', 'password1', 'password2', )
+
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(max_length=100,required=True,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    surname = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(required=True,widget=forms.TextInput(attrs={'class': 'form-control'}))
+    class Meta:
+        model = User
+        fields = ['username', 'name', 'surname', 'email',]
