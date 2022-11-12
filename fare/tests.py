77691fa53451c6acd7e4f1f8a85c1e67ccb55f72
6310682593 <6310682593@student.tu.ac.th>
2022-11-12 17:34:28 +0700

    complete fare

---
+++ fare/tests.py
@@ -0,0 +1,139 @@
+from django.test import TestCase, Client
+from doctest import REPORT_CDIFF
+from django.urls import reverse
+from users import views
+from urllib import request, response
+from django.contrib.auth.models import User
+from bts_for_fun.models import Station
+
+# Create your tests here.
+
+class FarePageTests(TestCase):
+    def setUp(self):
+            #create user
+            self.user = User.objects.create_user('test_user', password='test_pass')
+            station1 = Station.objects.create(thai_name_station='คูคต', eng_name_station='Khu Khot', code_station='N24', station_detail='-', id=1)
+            station2 = Station.objects.create(thai_name_station='แยก คปอ.', eng_name_station='Yaek Kor Por Aor', code_station='N23', station_detail='-', id=2)
+            station3 = Station.objects.create(thai_name_station='สยาม', eng_name_station='Siam Station', code_station='CEN', station_detail='-', id=25)
+            station4 = Station.objects.create(thai_name_station='ราชเทวี', eng_name_station='Ratchathewi', code_station='N1', station_detail='-', id = 24)
+            station5 = Station.objects.create(thai_name_station='พญาไท', eng_name_station='Phaya Thai', code_station='N2', station_detail='-', id=23)
+            station6 = Station.objects.create(thai_name_station='อนุสาวรีย์ชัยสมรภูมิ', eng_name_station='Victory Monument', code_station='N3', station_detail='-', id=22)
+            station7 = Station.objects.create(thai_name_station='สนามเป้า', eng_name_station='Sanam Pao', code_station='N4', station_detail='-', id=21)
+            station8 = Station.objects.create(thai_name_station='อารีย์', eng_name_station='Ari', code_station='N5', station_detail='-', id=20)
+            station9 = Station.objects.create(thai_name_station='เสนาร่วม', eng_name_station='Sena Rua', code_station='N6', station_detail='-', id=19)
+            station10 = Station.objects.create(thai_name_station='สะพานควาย', eng_name_station='Saphan Khwai', code_station='N7', station_detail='-', id=18)
+            station11 = Station.objects.create(thai_name_station='หมอชิต', eng_name_station='Mo Chit', code_station='N8', station_detail='-', id=17)
+            station12 = Station.objects.create(thai_name_station='อ่อนนุช', eng_name_station='On Nut', code_station='E9', station_detail='-', id=34)
+            station13 = Station.objects.create(thai_name_station='บางจาก', eng_name_station='Bang Chak', code_station='E10', station_detail='-', id=35)
+
+    def test_login_view_status_code_from_fare_page(self):
+        """ login view's status code is ok """
+
+        c = Client()
+        response = c.get(reverse('fare:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_view_status_code_from_result_page(self):
+        """ login view's status code is ok """
+
+        c = Client()
+        response = c.get(reverse('fare:result'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_fare_view_status_code(self):
+        """ fare view's status code is ok """
+
+        c = Client()
+        c.login(username='test_user', password='test_pass')
+        response = c.get(reverse('fare:index'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_result_view_status_code(self):
+        """ result view's status code is ok """
+
+        c = Client()
+        c.login(username='test_user', password='test_pass')
+        entry = 1
+        destination = 2
+        response = c.post(reverse('fare:result'), {'entry' : entry, 'destination' : destination,})
+        self.assertEqual(response.status_code, 200)
+
+    def test_result_view_condition_1(self):
+        c = Client()
+        c.login(username='test_user', password='test_pass')
+        entry = 34
+        destination = 35
+        response = c.post(reverse('fare:result'), {'entry' : entry, 'destination' : destination,})
+        self.assertEqual(response.status_code, 200)
+
+    def test_result_view_condition_2(self):
+        c = Client()
+        c.login(username='test_user', password='test_pass')
+        entry = 25
+        destination = 24
+        response = c.post(reverse('fare:result'), {'entry' : entry, 'destination' : destination,})
+        self.assertEqual(response.status_code, 200)
+
+    def test_result_view_condition_3(self):
+        c = Client()
+        c.login(username='test_user', password='test_pass')
+        entry = 25
+        destination = 23
+        response = c.post(reverse('fare:result'), {'entry' : entry, 'destination' : destination,})
+        self.assertEqual(response.status_code, 200)
+
+    def test_result_view_condition_4(self):
+        c = Client()
+        c.login(username='test_user', password='test_pass')
+        entry = 25
+        destination = 22
+        response = c.post(reverse('fare:result'), {'entry' : entry, 'destination' : destination,})
+        self.assertEqual(response.status_code, 200)
+
+    def test_result_view_condition_5(self):
+        c = Client()
+        c.login(username='test_user', password='test_pass')
+        entry = 25
+        destination = 21
+        response = c.post(reverse('fare:result'), {'entry' : entry, 'destination' : destination,})
+        self.assertEqual(response.status_code, 200)
+
+    def test_result_view_condition_6(self):
+        c = Client()
+        c.login(username='test_user', password='test_pass')
+        entry = 25
+        destination = 20
+        response = c.post(reverse('fare:result'), {'entry' : entry, 'destination' : destination,})
+        self.assertEqual(response.status_code, 200)
+
+    def test_result_view_condition_7(self):
+        c = Client()
+        c.login(username='test_user', password='test_pass')
+        entry = 25
+        destination = 19
+        response = c.post(reverse('fare:result'), {'entry' : entry, 'destination' : destination,})
+        self.assertEqual(response.status_code, 200)
+
+    def test_result_view_condition_8(self):
+        c = Client()
+        c.login(username='test_user', password='test_pass')
+        entry = 25
+        destination = 18
+        response = c.post(reverse('fare:result'), {'entry' : entry, 'destination' : destination,})
+        self.assertEqual(response.status_code, 200)
+
+    def test_result_view_condition_9(self):
+        c = Client()
+        c.login(username='test_user', password='test_pass')
+        entry = 25
+        destination = 17
+        response = c.post(reverse('fare:result'), {'entry' : entry, 'destination' : destination,})
+        self.assertEqual(response.status_code, 200)
+
+    def test_result_view_condition_10(self):
+        c = Client()
+        c.login(username='test_user', password='test_pass')
+        entry = 1
+        destination = 34
+        response = c.post(reverse('fare:result'), {'entry' : entry, 'destination' : destination,})
+        self.assertEqual(response.status_code, 200)
---
+++ fare/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from bts_for_fun.models import Station
+import math
+
+# Create your views here.
+
+def index(request):
+    if not request.user.is_authenticated:
+        return render(request, 'users/login.html')
+    else:
+        
+        stations = Station.objects.all()
+        return render(request, 'fare/index.html',{
+                   'stations' : stations,
+    })
+
+def result(request):
+    if not request.user.is_authenticated:
+        return render(request, 'users/login.html')
+    else:
+        entry = request.POST['entry']
+        destination = request.POST['destination']
+        name_entry = Station.objects.get(id=entry)
+        name_destination = Station.objects.get(id=destination)
+        data = abs(int(destination) - int(entry))
+
+        if (int(entry) in range(1, 18)) and (int(destination) in range(1, 18)):
+            result = 0
+
+        elif (int(entry) in range(34, 49) and (int(destination) in range(34, 49))):
+            result = 15
+        
+        elif data == 1:
+            result = 16
+
+        elif data == 2:
+            result = 23
+
+        elif data == 3:
+            result = 26
+        
+        elif data == 4:
+            result = 30
+        
+        elif data == 5:
+            result = 33
+
+        elif data == 6:
+            result = 37
+
+        elif data == 7:
+            result = 40
+
+        elif 8 <= data <= 15:
+            result = 44
+        
+        elif data >= 16:
+            result = 59
+
+        return render(request, 'fare/result.html',{
+                   'result' : result,
+                   'entry' : name_entry,
+                   'destination' : name_destination,
+        })
