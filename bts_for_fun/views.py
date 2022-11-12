981eb11bf07e42cc9316d40ad01e4e4eb435c21a
Chanathinad Phispratuang <6310610941@cn103>
2022-11-12 23:04:28 +0700

    Tourist

--- bts_for_fun/views.py
+++ bts_for_fun/views.py
@@ -18,8 +18,21 @@ def stationdetail(request,station_id):
         return render(request, 'users/login.html')
     else:
         station = Station.objects.get(id=station_id)
-
+        tourists = Tourist.objects.filter(on_station=station)
         return render(request, 'bts_for_fun/stationdetail.html',{
                    'station' : station,
+                   'tourists' : tourists,
+
+    })
+
+
+def touristdetail(request,tourist_id):
+    if not request.user.is_authenticated:
+        return render(request, 'users/login.html')
+    else:
+        tourist = Tourist.objects.get(id=tourist_id)
+
+        return render(request, 'bts_for_fun/touristdetail.html',{
+                   'tourist' : tourist,
 
     })
