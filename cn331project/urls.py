a72d77d70e32fd90e895df7424c3b6ba7dd30792
6310682593 <69451679+6310682593@users.noreply.github.com>
2022-11-13 11:13:22 +0700

    Update urls.py

--- cn331project/urls.py
+++ cn331project/urls.py
@@ -22,6 +22,5 @@ urlpatterns = [
     path('bts_for_fun/', include('bts_for_fun.urls')),
     path('fare/', include('fare.urls', namespace='fare')),
     path('users/', include('users.urls', namespace='users')),
-    path('fare/', include('fare.urls', namespace='fare')),
 ]
 
