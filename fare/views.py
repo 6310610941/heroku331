089ec967ae9dbc5f65d3d6bd90bfa37e444c9be9
6310682593 <6310682593@student.tu.ac.th>
2022-11-13 13:10:46 +0700

    update fare

--- fare/views.py
+++ fare/views.py
@@ -23,17 +23,20 @@ def result(request):
         name_destination = Station.objects.get(id=destination)
         data = abs(int(destination) - int(entry))
 
-        if (int(entry) in range(1, 18)) and (int(destination) in range(1, 18)):
-            result = 0
-        elif (int(entry) in range(34, 49) and (int(destination) in range(34, 49))):
-            result = 15
-        elif (int(entry) in range(1, 18)):
+        
+        if (int(entry) in range(1, 18)) and (int(destination) not in range(1,18)):
             data = abs(int(destination) - 17)
 
-        elif (int(destination) in range(1, 18)):
+        elif (int(destination) in range(1, 18)) and (int(entry) not in range(1,18)):
             data = abs(17 - int(entry))
         
-        if data == 1:
+        if (int(entry) in range(1, 18)) and (int(destination) in range(1, 18)):
+            result = 0
+            
+        elif (int(entry) in range(34, 49) and (int(destination) in range(34, 49))):
+            result = 15
+
+        elif data == 1:
             result = 16
 
         elif data == 2:
