1709e1ec1a89fef29779eec5bb94c9c316bbf987
Chanathinad Phispratuang <6310610941@cn103>
2022-11-19 07:27:37 +0700

    link to tourist detail

--- bts_for_fun/urls.py
+++ bts_for_fun/urls.py
@@ -7,5 +7,6 @@ app_name = 'bts_for_fun'
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('<int:station_id>',views.stationdetail,name='stationdetail'),
+    path('stationdetail/<int:station_id>',views.stationdetail,name='stationdetail'),
+    path('touristdetail/<int:tourist_id>',views.touristdetail,name='touristdetail')
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
